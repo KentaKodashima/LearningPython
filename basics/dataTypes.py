f0609d0f137699f2a195658d3d04a4a1b9b51603
Kenta Kodashima <kentakodashima@gmail.com>
2021-07-14 12:59:51 -0700

    Add notes about string formatting in Python.

--- basics/dataTypes.py
+++ basics/dataTypes.py
@@ -18,6 +18,7 @@ mystring[3:6] # Start from "def"
 print(mystring[::2]) # acegikm
 mystring[::-1] # Reverse string
 
+
 # Strings are immutable
 name = 'Sam'
 # name[0] = 'P' doesn't work
@@ -25,7 +26,29 @@ name = 'Sam'
 last_letters = name[1:]
 new_name = 'P' + last_letters # 'Pam'
 
+
 # String concatenation
 # Can be multiplied
 letter = 'z'
 letter * 10 # 'zzzzzzzzzz'
+
+
+# String interpolation
+# Insert a string using .format()
+# The curly braces are replaced with the variable
+print('this is a string {}'.format('INSERTED'))
+# You can pass an index of a variable
+print('this {2} {1} {0}'.format('fox', 'brown', 'quick')) # The quick brown fox
+print('this {q} {b} {f}'.format(f='fox',b='brown',q='quick')) # The quick brown fox
+
+# Float formatting
+# {value:width.precision f}
+#   value: base value
+#   width: the length of the string to be
+#   precision: the length of the numbers after the decimal point
+result = 100/777 # 0.1287001287001287
+print('The result was {r:1.3f}'.format(r=result)) # 0.129
+
+# f_string
+name = 'Jose'
+print(f'The name is {name}')
