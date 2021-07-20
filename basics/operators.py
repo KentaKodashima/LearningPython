9a37d3234d5200b708474fb9c8723bb9e6ccef54
Kenta Kodashima <kentakodashima@gmail.com>
2021-07-20 11:45:33 -0700

    Add notes about some operators in Python.

--- basics/operators.py
+++ basics/operators.py
@@ -24,6 +24,7 @@
 # Example: (a <= b) is true.
 
 
+
 # ==========
 # Logical operators
 # ==========
@@ -38,3 +39,106 @@
 # Operator: not
 # Description: If the conditions is false
 # Example: not 1 == 1
+
+
+
+# ==========
+# Useful operators
+# ==========
+# range() operator
+# range(start,end,step)
+
+mylist = [1,2,3]
+for num in range(10):
+  # Print 0 to 9
+  print(num)
+
+for num in range(2, 10):
+  # Print 2 to 9
+  print(num)
+
+for num in range(0,11,2):
+  # Print 0,2,4,6,8,10
+  print(num)
+
+# list() operator
+# Generate a list based on the returning value of range
+list(range(0,11,2))
+
+# enumerate() operator
+# Return a tuples of an index and value
+
+# Output of the following code
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
+# (3, 'd')
+# (4, 'e')
+# (5, 'f')
+# (6, 'g')
+word = 'abcdefg'
+for item in enumerate(word):
+  print(item)
+
+for index, char in enumerate(word):
+  print(index)
+  print(char)
+  print('\n')
+
+# zip() operator
+# Zip together multiple lists and return as tuples
+
+# The following code's output
+# (1, 'a')
+# (2, 'b')
+# (3, 'c')
+mylist2 = [1,2,3]
+mylist3 = ['a','b','c']
+for item in zip(mylist2, mylist3):
+  print(item)
+
+# The tuples are fit into the shortest list
+# Any extra values will be ignored
+# The following code's output
+# (1, 'a', 10)
+# (2, 'b', 11)
+# (3, 'c', 12)
+mylist4 = [10,11,12,13,14]
+for item in zip(mylist2, mylist3, mylist4):
+  print(item)
+
+list(zip(mylist2, mylist3, mylist4))
+
+# in operator
+# Similar to include() in JS, it checks if a value exists in a list or an object
+'x' in [1,2,3] # false
+'x' in ['x', 'y', 'z'] # true
+'x' in 'xyz' # true
+'key' in {'key': 100} # true
+d = {'key': 100}
+100 in d.values # true
+'key' in d.keys # true
+
+# min/max operator
+mylist5 = [10,20,30,40,100]
+min(mylist5) # return 10
+max(mylist5) # return 100
+
+# Improting functions from Python's built-in library
+from random import shuffle
+mylist6 = [1,2,3,4,5,6,7,8,9,10]
+# shuffles a list
+shuffle(mylist6)
+
+from random import randint
+# Generate random int
+# randint(min,max)
+randint(0,100)
+
+# Getting user input
+# Asks the user for an input and store the resutl in a variable
+# The result will be string
+# To get an int, you need to cast using float(), int(), etc
+result = input('Enter a number here: ')
+int(result)
+float(result)
--- basics/statements.py
+++ basics/statements.py
@@ -1,4 +1,7 @@
+# Only the Python unique features are noted
+
 # if/else statement
+# No curly braces unline in JS
 a = 3
 b = 2
 
@@ -36,3 +39,19 @@ for (a, b) in tups:
 obj = {'0': 1, '1': 2, '2': 3}
 for val in obj.values:
   print(val) # 1 2 3
+
+# Keywords for loops
+# break: Ends the loop just like in JS
+
+# continue: Goes to the top of the closest enclosing loop
+str2 = 'Hello'
+for char in str2:
+  if char == 'l':
+    continue
+    # Continue to the next char without printing 'l'
+  print(char)
+
+# pass: Used to have empty content inside a loop
+x = [1,2,3]
+for item in x:
+  pass
