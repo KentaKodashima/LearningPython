d8f5f1efba76c837505d2632aa4d42be6b121375
Kenta Kodashima <kentakodashima@gmail.com>
2021-07-22 11:54:47 -0700

    Add notes about functions.

--- basics/functionsAndMethonds.py
+++ basics/functionsAndMethonds.py
@@ -30,3 +30,30 @@ def employee_check(work_hours):
 # The function returns a tuple
 # It can be unpacked as below
 name,hours = employee_check(work_hours)
+
+
+
+# ==========
+# Arguments
+# ==========
+# *: args
+# **: kwargs = keyword args
+
+# The function below cam take arbitary numbers of arguments
+# Args are treated as tuples inside the funciton
+def myfunc(*args):
+  return sum(args) * 0.05
+myfunc(40,60,100)
+myfunc(40,60,100,200)
+
+# THe function below can also take arbitary numbers of arguments
+# The args need to have a keyword
+# Insted of tuples, args would be dictionaries
+def myfunc(**kwargs):
+  if 'fruit' in kwargs:
+    print(f'My fruit of choice is {kwargs["fruit"]}')
+myfunc(fruit='apple',veggie='onions')
+
+def myfunc(*args,**kwargs):
+  print(f'I would like {args[0]} {kwargs["food"]}')
+myfunc(10,20,30,food='pizza',fruit='apple')
