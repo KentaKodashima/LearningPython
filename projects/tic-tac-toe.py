cfc9e378a0c5434005d58cfcfde0a450b8770b66
Kenta Kodashima <kentakodashima@gmail.com>
2021-08-03 14:23:19 -0700

    Working on a practice project.

---
+++ projects/tic-tac-toe.py
@@ -0,0 +1,37 @@
+import random
+
+board = [
+          ['','',''],
+          ['','',''],
+          ['','','']
+        ]
+
+def clear_output():
+  print('\n'*100)
+
+def display_board(board):
+  pass
+
+def player_input():
+  pass
+
+def place_marker(board, marker, position):
+  pass
+
+def win_check(board, mark):
+  pass
+
+def choose_first():
+  pass
+
+def space_check(board, position):
+  pass
+
+def full_board_check(board):
+  pass
+
+def player_choice(board):
+  pass
+
+def replay():
+  pass
